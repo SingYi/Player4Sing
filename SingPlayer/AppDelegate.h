81681244f8ed583458b696f9546ac4d967645c5a
SingYi <23323231@qq.com>
2016-08-06 15:01:54 +0800

    Initial Commit

---
+++ SingPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SingPlayer
+//
+//  Created by 石燚 on 16/8/6.
+//  Copyright © 2016年 SingYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SingPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SingPlayer
+//
+//  Created by 石燚 on 16/8/6.
+//  Copyright © 2016年 SingYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
